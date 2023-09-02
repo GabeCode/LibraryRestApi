12f2274b4a6e47cc78c846dbd4c2f144c416c6a2
Gabriel Abrego <gabrielblanco.gabo@gmail.com>
2023-09-01 22:13:04 -0600

    Adding domain classes

---
+++ src/main/java/com/gabrego/libraryrestapi/domain/Book.java
@@ -0,0 +1,25 @@
+package com.gabrego.libraryrestapi.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    private String isbn;
+
+    private String title;
+
+    @ManyToOne(cascade = CascadeType.ALL) //if author does not exist, it will be created
+    @JoinColumn(name = "author_id", referencedColumnName = "id")
+    private Author author;
+}
