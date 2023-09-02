2701fdf9604f9994325c3a5f6768e56ceec1b862
Gabriel Abrego <gabrielblanco.gabo@gmail.com>
2023-09-01 22:09:40 -0600

    Rest API Set Up

---
+++ src/test/java/com/gabrego/libraryrestapi/LibraryRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrego.libraryrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
